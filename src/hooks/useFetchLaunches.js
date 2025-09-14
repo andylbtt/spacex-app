4c291e30ff740e197071c8c4d498765b1e22a8b2
Andrea Lopez <andreac.lopezbau@gmail.com>
2025-09-14 14:58:49 -0600

    Fix: eliminar import duplicado en useFetchLaunches

--- src/hooks/useFetchLaunches.js
+++ src/hooks/useFetchLaunches.js
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import { fetchLaunches } from "../services/spaceXService";
-
 import { useState, useEffect } from "react";
 import { fetchLaunches } from "../services/spaceXService";
 const useFetchLaunches = () => {
