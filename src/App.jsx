8dcf8ace617c291b2370a7e1e047a995ad119d6f
Andrea Lopez <andreac.lopezbau@gmail.com>
2025-09-14 15:30:14 -0600

    listo todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import PrivateRoute from "./routes/PrivateRoute";
-
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Launches from "./pages/Launches";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/launches" element={<Launches />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/launches" element={<Launches />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
   );
 }
 
 export default App;
+
 
